b3451b36d59e867320bb2e9e289d04c5b45f4669
LucienLachancee <87431597+LucienLachancee@users.noreply.github.com>
2025-09-10 16:54:29 +0200

    ajout dashboard metrics avec modif url/views

--- dream_bridge/dream_bridge_app/urls.py
+++ dream_bridge/dream_bridge_app/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path('dashboard/', views.dashboard, name='dashboard'),
     path('narrate/', dream_create_view, name='narrate'),
     path('dreams/<uuid:dream_id>/status/', dream_status_view, name='dream-status'),
+    path('report/', views.dashboard_view, name='report'),
     path('galerie/', views.galerie_filtrée, name='galerie'),
 
 ]
