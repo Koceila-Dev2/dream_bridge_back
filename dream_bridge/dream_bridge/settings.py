c9713308341013e84f5517c3f10a1524f9295f05
Clément Asensio <124190865+macfind-code@users.noreply.github.com>
2025-09-18 11:52:51 +0200

    UX V1
    Accueil et page user -> Done
    Reste à faire =
    - Footer
    - Hover souris
    - Redirection de déconnexion

--- dream_bridge/dream_bridge/settings.py
+++ dream_bridge/dream_bridge/settings.py
@@ -256,9 +256,7 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
+STATICFILES_DIRS = [BASE_DIR / "dream_bridge_app" / "static"]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
